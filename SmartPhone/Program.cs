e4179eabf21071f0cff18e627992f7b9cba9b7cc
afifmaulanaanam1-cmd <afifmaulanaanam1@gmail.com>
2026-04-17 10:27:18 +0700

    mempelajari interface ISmartPhone

--- SmartPhone/Program.cs
+++ SmartPhone/Program.cs
@@ -1,2 +1,88 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿ISmartPhone hpBudi = new iPhone();
+
+Fotographer fotografer = new Fotographer();
+fotografer.Kerja(hpBudi);
+
+hpBudi = new Samsung();
+fotografer.Kerja(hpBudi);
+
+
+interface ISmartPhone
+{
+    public void AmbilFoto();
+}
+
+class iPhone : ISmartPhone
+{
+    public void AmbilFoto()
+    {
+        Console.WriteLine("Mengaktifkan sensor kamera premium....*Cekrek!*");
+    }
+}
+
+class Samsung : ISmartPhone
+{
+    public void AmbilFoto()
+    {
+        Console.WriteLine("Mengaktifkan fitur beauty.... *Cekrek!*");
+    }
+}
+
+class Fotographer
+{
+   public void Kerja(ISmartPhone hp)
+    {
+        hp.AmbilFoto();
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
